57908ae612f7986ae3a48b9dc79b40318edb6076
trickypr <trickypr@icloud.com>
2022-01-16 13:10:34 +1100

    💄 Add button component

---
+++ src/views/techExperience/index.tsx
@@ -0,0 +1,11 @@
+export const TechExperience = () => (
+  <div>
+    <h1>Tech Experience</h1>
+    <p>
+      Different versions of linux are optimise for different levels of
+      experience. Most users want linux to just work for them, however, some
+      specific users may want to access some more complex functions that would
+      reduce stability for regular users
+    </p>
+  </div>
+)
