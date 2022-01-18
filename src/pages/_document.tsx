12dad000744fc93c335bb78567ddc671b876f381
trickypr <trickypr@icloud.com>
2022-01-18 12:20:49 +1100

    💄 Set default font to jost

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -17,6 +17,13 @@ class MyDocument extends Document {
       <Html>
         <Head>
           <meta charSet="UTF-8" />
+
+          <link rel="preconnect" href="https://fonts.googleapis.com" />
+          <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin />
+          <link
+            href="https://fonts.googleapis.com/css2?family=Jost:wght@400;700&display=swap"
+            rel="stylesheet"
+          />
         </Head>
         <body>
           <Main />
